094b0729bcddfd23a3a283fef5a604cb5e92faf7
satywanKumar <48269217+satywanKumar@users.noreply.github.com>
2023-03-07 21:07:35 +0530

    change

--- api/routes/category.js
+++ api/routes/category.js
@@ -6,9 +6,9 @@ const cloudinary = require('cloudinary').v2;
 
 
 cloudinary.config({
-  cloud_name:'dmap97ds2',
-  api_key:'392635253424173',
-  api_secret:'UwIU2QdVR_S7KCsxfnxKEtLJ64k'
+  cloud_name:'',
+  api_key:'',
+  api_secret:''
 });
 
 

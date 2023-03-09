243ee359eee3756df4f4d3d9bd3b483f6fad2c05
satywanKumar <48269217+satywanKumar@users.noreply.github.com>
2023-03-09 12:35:53 +0530

    detail updated

--- api/model/user.js
+++ api/model/user.js
@@ -1,16 +1,10 @@
 const mongoose = require('mongoose');
 userSchema = mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
-    firstName:String,
-    lastName:String,
+    userName:String,
     password:String,
     phone:Number,
     email:String,
-    address:String,
-    city:String,
-    state:String,
-    pin:String,
-    userType:String
 })
 
 module.exports = mongoose.model('user',userSchema);
